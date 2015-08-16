27f0be04d8d1fe5a737653a1fe16e6f930ced8df
Thomas Levine <_@thomaslevine.com>
2015-08-16 15:11:33 -0400

    all randomized

--- uurree/test/test_uurree.py
+++ uurree/test/test_uurree.py
@@ -4,13 +4,8 @@ import pytest
 
 from .. import uurree
 
-find_line_start_testcases = [
-    (88, 0), (143, 0), (0, 0), (1321231, 0),
-]
-
 @pytest.mark.randomize(min_num = -10, max_num = 15382 * 2)
-@pytest.mark.parametrize('seed, expected_line_start', find_line_start_testcases)
-def test_find_line_start(seed, expected_line_start, interval:int):
+def test_find_line_start(seed:int, interval:int):
     fn = os.path.abspath(os.path.join(__file__, '..', 'fixtures', 'parsing-pdfs.md'))
     with open(fn) as fp:
         fp.seek(seed)
@@ -25,5 +20,4 @@ def test_find_line_start(seed, expected_line_start, interval:int):
 
     assert prev_char == '\n'
     assert line_start < seed < line_end
-   #assert observed == expected_line_start
     
