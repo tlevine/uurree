460842b3dee6c003e17fcd1ba0b1f665ab211c72
Thomas Levine <_@thomaslevine.com>
2015-08-16 14:52:49 -0400

    get the test working

--- uurree/test/test_uurree.py
+++ uurree/test/test_uurree.py
@@ -1,7 +1,14 @@
 import os
 
+import pytest
+
 from .. import uurree
 
+find_line_start_testcases = [
+    (88, 0), (143, 0), (0, 0), (1321231, 0),
+]
+
+@pytest.mark.parametrize('seed, line_start', find_line_start_testcases)
 def test_find_line_start(seed, line_start):
     fn = os.path.abspath(os.path.join(__file__, '..', 'fixtures', 'parsing-pdfs.md'))
     with open(fn) as fp:
