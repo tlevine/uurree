629839e91ac876f9a4b522e2b43bfa0c775b4123
Thomas Levine <_@thomaslevine.com>
2015-08-16 14:51:06 -0400

    write a test better

--- uurree/test/test_uurree.py
+++ uurree/test/test_uurree.py
@@ -1,2 +1,11 @@
+import os
+
+from .. import uurree
+
 def test_find_line_start(seed, line_start):
+    fn = os.path.abspath(os.path.join(__file__, '..', 'fixtures', 'parsing-pdfs.md'))
+    with open(fn) as fp:
+        fp.seek(seed)
+        observed = uurree.find_line_start(fp)
+    assert observed == line_start
     
