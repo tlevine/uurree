4a2c00b4d593c89ffbcf37f45eb174064adfd130
Thomas Levine <_@thomaslevine.com>
2015-08-16 17:14:38 -0400

    rename

--- uurree/test/test_stratified_sample.py
+++ uurree/test/test_stratified_sample.py
@@ -3,13 +3,13 @@ import os, signal, logging
 from more_itertools import ilen, consume
 import pytest
 
-from .. import simple_random_sample
+from .. import stratified_sample
 
 logging.basicConfig(level = logging.DEBUG)
 fn = os.path.abspath(os.path.join(__file__, '..', 'fixtures', 'parsing-pdfs.md'))
 
 @pytest.mark.randomize(min_num = -10, max_num = 100, ncalls = 10)
-def test_simple_random(n:int, replace:bool):
+def test_stratified(n:int, replace:bool):
     with open(fn) as fp:
         # The function should finish quickly.
         def abort(*args):
@@ -19,10 +19,10 @@ def test_simple_random(n:int, replace:bool):
 
         # We should get the right number of results.
         if n >= 0:
-            observed = ilen(simple_random_sample.sample(n, fp, replace = replace, give_up_at = 100))
+            observed = ilen(stratified_sample.sample(n, fp, replace = replace, give_up_at = 100))
             assert observed == n
         else:
             with pytest.raises(ValueError):
-                consume(simple_random_sample.sample(n, fp))
+                consume(stratified_sample.sample(n, fp))
 
         signal.alarm(0)
