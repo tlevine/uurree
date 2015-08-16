75d2958dfac3a9f993414afcff566a27b023eada
Thomas Levine <_@thomaslevine.com>
2015-08-16 15:06:14 -0400

    sane intervals

--- uurree/test/test_uurree.py
+++ uurree/test/test_uurree.py
@@ -8,11 +8,9 @@ find_line_start_testcases = [
     (88, 0), (143, 0), (0, 0), (1321231, 0),
 ]
 
-@pytest.mark.randomize()
+@pytest.mark.randomize(min_num = -10, max_num = 15382 * 2)
 @pytest.mark.parametrize('seed, line_start', find_line_start_testcases)
 def test_find_line_start(seed, line_start, interval:int):
-    print(interval)
-    interval = 100
     fn = os.path.abspath(os.path.join(__file__, '..', 'fixtures', 'parsing-pdfs.md'))
     with open(fn) as fp:
         fp.seek(seed)
